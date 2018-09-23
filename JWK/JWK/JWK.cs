c9f4ba724f18683c0666ba8515d39c2f0852249a
Alexander Zautke <mail@alexander-zautke.com>
2018-09-23 18:48:17 +0200

    Create stubs for creating JWKs

---
+++ JWK/JWK/JWK.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Newtonsoft.Json;
+using JWK.Contants;
+
+namespace JWK
+{
+    public class JWK
+    {
+        [JsonProperty(PropertyName = "kty")]
+        private KeyType keyType;                // REQUIRED
+
+        [JsonProperty(PropertyName = "use")]
+        private PublicKeyUse publicKeyUse;      // OPTIONAL
+
+        [JsonProperty(PropertyName = "key_ops")]
+        private KeyOperations keyOperations;    // OPTIONAL
+
+        [JsonProperty(PropertyName = "alg")]
+        private Algorithm algorithm;            // OPTIONAL
+
+        [JsonProperty(PropertyName = "kid")]
+        private Guid keyID;                     // OPTIONAL
+
+        public string JWKfromOptions(PublicKeyUse publicKeyUse, KeyOperations keyOperations, Algorithm algorithm)
+        {
+            this.publicKeyUse = publicKeyUse;
+            this.keyOperations = keyOperations;
+            this.algorithm = algorithm;
+            this.keyID = Guid.NewGuid();
+            this.keyType = algorithm.KeyType;
+
+            if(algorithm.KeyType.Equals(KeyType.EllipticCurve)){
+                throw new NotImplementedException("Elliptic Curve Key Parameters are not yet supported");
+            }
+            else if(algorithm.KeyType.Equals(KeyType.RSA)){
+                throw new NotImplementedException("RSA Key Parameters are not yet supported");
+            }
+            else if (algorithm.KeyType.Equals(KeyType.HMAC))
+            {
+                throw new NotImplementedException("HMAC Key Parameters are not yet supported");
+            }
+            else if (algorithm.KeyType.Equals(KeyType.AES))
+            {
+                throw new NotImplementedException("AES Key Parameters are not yet supported");
+            }
+            else{
+                throw new NotImplementedException("None Key Type is not yet supported");
+            }
+
+            return JsonConvert.SerializeObject(this);
+        }
+
+        private void ECAParameters()
+        {
+
+        }
+
+        private void RSAParameters(){
+
+        }
+
+        private void HMACParameters()
+        {
+
+        }
+
+        private void AESParameters()
+        {
+
+        }
+
+        private void NONEParameters()
+        {
+
+        }
+
+    }
+
+}
---
+++ JWK/JWK/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JWK.Contants;
+
+namespace JWK
+{
+    public class Startup
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Implementation of JSON Web Keys (RFC7517)");
+
+            JWK jwk = new JWK();
+            PublicKeyUse keyUse = PublicKeyUse.Signature;
+            KeyOperations keyOperations = KeyOperations.ComputeDigitalSignature;
+            Algorithm algorithm = Algorithm.None;
+            string jwkString = jwk.JWKfromOptions(keyUse, keyOperations, algorithm);
+
+            Console.WriteLine(jwkString);
+        }
+    }
+}
