ddb7d5cdb263a90cf2efc8318f8fd33e306ba134
Alexander Zautke <mail@alexander-zautke.com>
2018-09-24 19:08:11 +0200

    Add KeyParameters
    Dict that will be filled with custom values for each Key Type

---
+++ JWK/JWK/KeyParts/KeyParameters.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using JWK.TypeConverters;
+
+namespace JWK.KeyParts
+{
+    [TypeConverter(typeof(ConstantConverter))]
+    public sealed class KeyParameters
+    {
+        private readonly Dictionary<string, string> values;
+
+        public KeyParameters(Dictionary<string, string> keyParameters){
+            values = keyParameters;
+        }
+
+        public override string ToString()
+        {
+            return values.Aggregate(new StringBuilder(),(result, 
+                                                         currentValue) => result.AppendFormat("\"{0}\":\"{1}\",", currentValue.Key, currentValue.Value), 
+                                                         sb => sb.ToString());
+        }
+    }
+}
