53ce17607ecc563740aa753841ef03d06de50773
Alexander Zautke <mail@alexander-zautke.com>
2018-10-07 15:47:40 +0200

    Remove unused dependencies

--- JWK/JWK/KeyParts/KeyParameters.cs
+++ JWK/JWK/KeyParts/KeyParameters.cs
@@ -1,9 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
-using JWK.TypeConverters;
-using Newtonsoft.Json;
 
 namespace JWK.KeyParts
 {
