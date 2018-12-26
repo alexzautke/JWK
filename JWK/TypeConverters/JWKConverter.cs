fb52eb7c9d95fb3e8211b890f15767d0e9cb159a
Alexander Zautke <mail@alexander-zautke.com>
2018-12-26 22:08:09 +0100

    Fix JWKConverter after declaring all properties (getter) as public

--- JWK/TypeConverters/JWKConverter.cs
+++ JWK/TypeConverters/JWKConverter.cs
@@ -27,12 +27,14 @@ namespace CreativeCode.JWK.TypeConverters
             _writer.WriteStartObject();
 
             var type = value.GetType();
-            FieldInfo[] fields = type.GetFields(BindingFlags.NonPublic|BindingFlags.Instance);
-            foreach (var field in fields)
+            var properties = type.GetProperties(); // Get all public properties
+            var head = properties.First();
+            foreach (var property in properties)
             {
-                var propertyValue = field.GetValue(value);
-                foreach (var customAttributeData in field.CustomAttributes){
+                var propertyValue = property.GetValue(value);
+                foreach (var customAttributeData in property.CustomAttributes){
 
+                    var test = typeof(JsonPropertyAttribute);
                     if (customAttributeData.AttributeType != typeof(JsonPropertyAttribute))
                     {
                         break; // Only serialize fields which are marked with "JsonProperty"
@@ -41,14 +43,14 @@ namespace CreativeCode.JWK.TypeConverters
                     if (customAttributeData.NamedArguments.Any()) // JWK class indicated a custom name
                     {
                         var customJSONPropertyName = customAttributeData.NamedArguments[0].TypedValue.ToString();
-                        WriteTrailingComma(fields, field);
+                        WriteTrailingComma(head, property);
                         _writer.WriteRaw(customJSONPropertyName + ":\"" + propertyValue + "\"");
                     }
                     else // Attribute handles JSON property name and formatting itself
                     {
                         if(propertyValue != null)
                         {
-                            WriteTrailingComma(fields, field);
+                            WriteTrailingComma(head, property);
                             _writer.WriteRaw(propertyValue.ToString());
                         }
                     }
@@ -58,8 +60,9 @@ namespace CreativeCode.JWK.TypeConverters
             _writer.WriteEndObject();
         }
 
-        private void WriteTrailingComma(FieldInfo[] fields, FieldInfo field){
-            if (field != fields.First()) // Don't start the JSON object with a comma
+        private void WriteTrailingComma(PropertyInfo head, PropertyInfo property)
+        {
+            if (property != head) // Don't start the JSON object with a comma
             {
                 _writer.WriteRaw(",");
             }
