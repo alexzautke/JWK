5bfd958b306a0e3f84b15c60d5c7725e8aef6057
Alexander Zautke <alexander@fire.ly>
2020-11-03 15:18:23 +0100

    KeyParameters - Make Values public

--- JWK/KeyParts/KeyParameters.cs
+++ JWK/KeyParts/KeyParameters.cs
@@ -7,15 +7,18 @@ namespace CreativeCode.JWK.KeyParts
 {
     public sealed class KeyParameters : IJWKKeyPart
     {
-        private readonly Dictionary<string, (string parameterValue, bool isPrivate)> values; // Dictionary<key, Tuple<value, isPrivate>>
+        public Dictionary<string, (string parameterValue, bool isPrivate)> Values { get; } // Dictionary<key, Tuple<value, isPrivate>>
 
         public KeyParameters(Dictionary<string, (string parameterValue, bool isPrivate)> keyParameters){
-            values = keyParameters;
+            if (keyParameters == null)
+                throw new ArgumentNullException("Key Parameters cannot be null");
+
+            Values = keyParameters;
         }
 
         public string Serialize(bool shouldExportPrivateKey = false)
         {
-            return values.Aggregate(new StringBuilder(), (result,
+            return Values.Aggregate(new StringBuilder(), (result,
                                                          currentParameter) => AppendKeyParameter(result, currentParameter, shouldExportPrivateKey), 
                                                          TrimTraillingComma);
         }
@@ -24,7 +27,7 @@ namespace CreativeCode.JWK.KeyParts
         {
             // Don't seralize empty JSON properties (i.e., private key parameters if "public key only" mode is requested)
             // Don't seralize if value is marked as private and shouldExportPrivateKey is set to false
-            if (currentParameter.Value.Item1 != string.Empty && (!(currentParameter.Value.isPrivate && !shouldExportPrivateKey)))
+            if (currentParameter.Value.parameterValue != string.Empty && (!(currentParameter.Value.isPrivate && !shouldExportPrivateKey)))
                 current.AppendFormat("\"{0}\":\"{1}\",", currentParameter.Key, currentParameter.Value.parameterValue);
 
             return current;
