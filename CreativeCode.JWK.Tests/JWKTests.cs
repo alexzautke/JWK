ae8b5db27fc3ebc202a171beffd7f614a9c1ce4f
Alexander Zautke <alexander@fire.ly>
2020-12-21 22:58:41 +0100

    Add unit tests for all RSA and EC key types

--- CreativeCode.JWK.Tests/JWKTests.cs
+++ CreativeCode.JWK.Tests/JWKTests.cs
@@ -27,6 +27,12 @@ namespace CreativeCode.JWK.Tests
             parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("n", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("e", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("p", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("q", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dq", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dp", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("qi", out var _).Should().BeTrue();
 
             parsedJWK.GetValue("kty").ToString().Should().Be("RSA");
             parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.RS256.Name);
@@ -53,6 +59,12 @@ namespace CreativeCode.JWK.Tests
             parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("n", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("e", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("p", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("q", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dq", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dp", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("qi", out var _).Should().BeTrue();
 
             parsedJWK.GetValue("kty").ToString().Should().Be("RSA");
             parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.RS384.Name);
@@ -79,6 +91,12 @@ namespace CreativeCode.JWK.Tests
             parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("n", out var _).Should().BeTrue();
             parsedJWK.TryGetValue("e", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("p", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("q", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dq", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("dp", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("qi", out var _).Should().BeTrue();
 
             parsedJWK.GetValue("kty").ToString().Should().Be("RSA");
             parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.RS512.Name);
@@ -87,9 +105,68 @@ namespace CreativeCode.JWK.Tests
             parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
         }
 
+        [Fact]
+        public void JWKCheckRSAPrivateKeyParametersExport()
+        {
+            JWK jwk = new JWK();
+            PublicKeyUse keyUse = PublicKeyUse.Signature;
+            KeyOperations keyOperations = new KeyOperations(new[] { KeyOperations.ComputeDigitalSignature, KeyOperations.VerifyDigitalSignature });
+            Algorithm algorithm = Algorithm.RS384;
+            jwk.BuildWithOptions(keyUse, keyOperations, algorithm);
+
+            string jwkString = jwk.Export(false);
+            var parsedJWK = JObject.Parse(jwkString);
+
+            parsedJWK.TryGetValue("kty", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("alg", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("use", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("n", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("e", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeFalse();
+            parsedJWK.TryGetValue("p", out var _).Should().BeFalse();
+            parsedJWK.TryGetValue("q", out var _).Should().BeFalse();
+            parsedJWK.TryGetValue("dq", out var _).Should().BeFalse();
+            parsedJWK.TryGetValue("dp", out var _).Should().BeFalse();
+            parsedJWK.TryGetValue("qi", out var _).Should().BeFalse();
+
+            parsedJWK.GetValue("kty").ToString().Should().Be("RSA");
+            parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.RS384.Name);
+            parsedJWK.GetValue("use").ToString().Should().Be(PublicKeyUse.Signature.KeyUse);
+            parsedJWK.GetValue("key_ops").Values<string>().Count().Should().Be(2);
+            parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
+        }
 
         [Fact]
-        public void JWKWithECSignatureCanBeSerialized()
+        public void JWKWithEC256SignatureCanBeSerialized()
+        {
+            JWK jwk = new JWK();
+            PublicKeyUse keyUse = PublicKeyUse.Signature;
+            KeyOperations keyOperations = new KeyOperations(new[] { KeyOperations.ComputeDigitalSignature, KeyOperations.VerifyDigitalSignature });
+            Algorithm algorithm = Algorithm.ES256;
+            jwk.BuildWithOptions(keyUse, keyOperations, algorithm);
+
+            string jwkString = jwk.Export(true);
+            var parsedJWK = JObject.Parse(jwkString);
+
+            parsedJWK.TryGetValue("kty", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("alg", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("use", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("crv", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("x", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("y", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+
+            parsedJWK.GetValue("kty").ToString().Should().Be("EC");
+            parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.ES256.Name);
+            parsedJWK.GetValue("use").ToString().Should().Be(PublicKeyUse.Signature.KeyUse);
+            parsedJWK.GetValue("key_ops").Values<string>().Count().Should().Be(2);
+            parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
+        }
+
+        [Fact]
+        public void JWKWithEC384SignatureCanBeSerialized()
         {
             JWK jwk = new JWK();
             PublicKeyUse keyUse = PublicKeyUse.Signature;
@@ -99,6 +176,77 @@ namespace CreativeCode.JWK.Tests
 
             string jwkString = jwk.Export(true);
             var parsedJWK = JObject.Parse(jwkString);
+
+            parsedJWK.TryGetValue("kty", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("alg", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("use", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("crv", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("x", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("y", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+
+            parsedJWK.GetValue("kty").ToString().Should().Be("EC");
+            parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.ES384.Name);
+            parsedJWK.GetValue("use").ToString().Should().Be(PublicKeyUse.Signature.KeyUse);
+            parsedJWK.GetValue("key_ops").Values<string>().Count().Should().Be(2);
+            parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
+        }
+
+        [Fact]
+        public void JWKWithEC512SignatureCanBeSerialized()
+        {
+            JWK jwk = new JWK();
+            PublicKeyUse keyUse = PublicKeyUse.Signature;
+            KeyOperations keyOperations = new KeyOperations(new[] { KeyOperations.ComputeDigitalSignature, KeyOperations.VerifyDigitalSignature });
+            Algorithm algorithm = Algorithm.ES512;
+            jwk.BuildWithOptions(keyUse, keyOperations, algorithm);
+
+            string jwkString = jwk.Export(true);
+            var parsedJWK = JObject.Parse(jwkString);
+
+            parsedJWK.TryGetValue("kty", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("alg", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("use", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("crv", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("x", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("y", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeTrue();
+
+            parsedJWK.GetValue("kty").ToString().Should().Be("EC");
+            parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.ES512.Name);
+            parsedJWK.GetValue("use").ToString().Should().Be(PublicKeyUse.Signature.KeyUse);
+            parsedJWK.GetValue("key_ops").Values<string>().Count().Should().Be(2);
+            parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
+        }
+
+        [Fact]
+        public void JWKCheckECPrivateKeyParametersExport()
+        {
+            JWK jwk = new JWK();
+            PublicKeyUse keyUse = PublicKeyUse.Signature;
+            KeyOperations keyOperations = new KeyOperations(new[] { KeyOperations.ComputeDigitalSignature, KeyOperations.VerifyDigitalSignature });
+            Algorithm algorithm = Algorithm.ES256;
+            jwk.BuildWithOptions(keyUse, keyOperations, algorithm);
+
+            string jwkString = jwk.Export(false);
+            var parsedJWK = JObject.Parse(jwkString);
+
+            parsedJWK.TryGetValue("kty", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("alg", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("use", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("kid", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("crv", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("x", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("y", out var _).Should().BeTrue();
+            parsedJWK.TryGetValue("d", out var _).Should().BeFalse();
+
+            parsedJWK.GetValue("kty").ToString().Should().Be("EC");
+            parsedJWK.GetValue("alg").ToString().Should().Be(Algorithm.ES256.Name);
+            parsedJWK.GetValue("use").ToString().Should().Be(PublicKeyUse.Signature.KeyUse);
+            parsedJWK.GetValue("key_ops").Values<string>().Count().Should().Be(2);
+            parsedJWK.GetValue("key_ops").Values<string>().Should().BeEquivalentTo(new[] { KeyOperations.ComputeDigitalSignature.Operations.FirstOrDefault(), KeyOperations.VerifyDigitalSignature.Operations.FirstOrDefault() });
         }
     }
 }
