83f3cbfbc35bc363290de233977883d19fbdafaa
Alexander Zautke <alexander@fire.ly>
2021-01-14 11:02:45 +0100

    Delete unnecessary test

--- CreativeCode.JWK.Tests/AlgorithmTests.cs
+++ CreativeCode.JWK.Tests/AlgorithmTests.cs
@@ -8,14 +8,6 @@ namespace CreativeCode.JWK.Tests
     public class AlgorithmTests
     {
         [Fact]
-        public void AlgorithmShouldReturnCorrectPart()
-        {
-            var algorithm = Algorithm.RS384;
-            algorithm.Name.Should().Be("RS384", "Name property should return the correct name of the algorithm");
-            algorithm.KeyType.Should().Be(KeyType.RSA, "Name property should return the correct name of the algorithm");
-        }
-
-        [Fact]
         public void AlgorithmCanBeSerialized()
         {
             var algorithm = Algorithm.RS384;
