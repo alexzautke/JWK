3bcbc2192d68bc81b1212a0960cc5ac537dd8ad7
Alexander Zautke <alexander@fire.ly>
2021-01-14 11:03:47 +0100

    Remove KeyType from Algorithm

--- JWK/KeyParts/Algorithm.cs
+++ JWK/KeyParts/Algorithm.cs
@@ -9,36 +9,38 @@ namespace CreativeCode.JWK.KeyParts
     public sealed class Algorithm : IJWKKeyPart
     {
         // HMAC
-        public static readonly Algorithm HS256 = new Algorithm("HS256", KeyType.HMAC);
-        public static readonly Algorithm HS384 = new Algorithm("HS384", KeyType.HMAC);
-        public static readonly Algorithm HS512 = new Algorithm("HS512", KeyType.HMAC);
+        public static readonly Algorithm HS256 = new Algorithm("HS256", true);
+        public static readonly Algorithm HS384 = new Algorithm("HS384", true);
+        public static readonly Algorithm HS512 = new Algorithm("HS512", true);
 
         // RSA
         // Support for PS256, PS384, PS512 is not planned.
-        public static readonly Algorithm RS256 = new Algorithm("RS256", KeyType.RSA);
-        public static readonly Algorithm RS384 = new Algorithm("RS384", KeyType.RSA);
-        public static readonly Algorithm RS512 = new Algorithm("RS512", KeyType.RSA);
+        public static readonly Algorithm RS256 = new Algorithm("RS256", false);
+        public static readonly Algorithm RS384 = new Algorithm("RS384", false);
+        public static readonly Algorithm RS512 = new Algorithm("RS512", false);
 
         // Elliptic Curve
-        public static readonly Algorithm ES256 = new Algorithm("ES256", KeyType.EllipticCurve);
-        public static readonly Algorithm ES384 = new Algorithm("ES384", KeyType.EllipticCurve);
-        public static readonly Algorithm ES512 = new Algorithm("ES512", KeyType.EllipticCurve);
+        public static readonly Algorithm ES256 = new Algorithm("ES256", false);
+        public static readonly Algorithm ES384 = new Algorithm("ES384", false);
+        public static readonly Algorithm ES512 = new Algorithm("ES512", false);
 
         // AES
-        public static readonly Algorithm A128GCMKW = new Algorithm("A128GCMKW", KeyType.AES);
-        public static readonly Algorithm A192GCMKW = new Algorithm("A192GCMKW", KeyType.AES);
-        public static readonly Algorithm A256GCMKW = new Algorithm("A256GCMKW", KeyType.AES);
+        public static readonly Algorithm A128GCMKW = new Algorithm("A128GCMKW", true);
+        public static readonly Algorithm A192GCMKW = new Algorithm("A192GCMKW", true);
+        public static readonly Algorithm A256GCMKW = new Algorithm("A256GCMKW", true);
 
         // None
-        public static readonly Algorithm None = new Algorithm("none", KeyType.None);
+        public static readonly Algorithm None = new Algorithm("none", false);
 
         public string Name { get; }
-        public KeyType KeyType { get; }
+        public bool IsSymetric { get; }
 
-        private Algorithm(string name, KeyType keyType)
+        private Algorithm() { } // Used only for deserialization
+
+        private Algorithm(string name, bool isSymetric)
         {
-            this.Name = name;
-            this.KeyType = keyType;
+            Name = name;
+            IsSymetric = isSymetric;
         }
 
         public string Serialize(bool shouldExportPrivateKey = false)
--- JWK/KeyParts/KeyType.cs
+++ JWK/KeyParts/KeyType.cs
@@ -5,11 +5,13 @@ namespace CreativeCode.JWK.KeyParts
     // See RFC 7518 - JSON Web Algorithms (JWA) - Section 6.1. "kty" (Key Type) Parameter Values
     public sealed class KeyType : IJWKKeyPart
     {
-        public static readonly KeyType EllipticCurve = new KeyType("EC");
-        public static readonly KeyType RSA = new KeyType("RSA");
-        public static readonly KeyType HMAC = new KeyType("oct");
-        public static readonly KeyType AES = new KeyType("oct");
-        public static readonly KeyType None = new KeyType("oct");
+        private const string EC_VALUE = "EC";
+        private const string RSA_VALUE = "RSA";
+        private const string OCT_VALUE = "OCT";
+
+        public static readonly KeyType EllipticCurve = new KeyType(EC_VALUE);
+        public static readonly KeyType RSA = new KeyType(RSA_VALUE);
+        public static readonly KeyType OCT = new KeyType(OCT_VALUE);
 
         public string Type;
 
