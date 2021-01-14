fe37ba3df57a5bacc9750e8cf1a5755d2aa65ea6
Alexander Zautke <alexander@fire.ly>
2021-01-14 21:22:53 +0100

    Add deserialization for KeyOperations

--- JWK/KeyParts/KeyOperations.cs
+++ JWK/KeyParts/KeyOperations.cs
@@ -10,14 +10,23 @@ namespace CreativeCode.JWK.KeyParts
     [JsonConverter(typeof(KeyOperationsConverter))]
     public sealed class KeyOperations : IJWKKeyPart
     {
-        public static readonly KeyOperations ComputeDigitalSignature = new KeyOperations("sign");
-        public static readonly KeyOperations VerifyDigitalSignature = new KeyOperations("verify");
-        public static readonly KeyOperations EncryptContent = new KeyOperations("encrypt");
-        public static readonly KeyOperations DecryptContentAndValidateDecryption = new KeyOperations("decrypt");
-        public static readonly KeyOperations EncryptKey = new KeyOperations("wrapKey");
-        public static readonly KeyOperations DecryptKeyAndValidateDecryption = new KeyOperations("unwrapKey");
-        public static readonly KeyOperations DeriveKey = new KeyOperations("deriveKey");
-        public static readonly KeyOperations DeriveBits = new KeyOperations("deriveBits");
+        private const string SIGN_VALUE = "sign";
+        private const string VERIFY_VALUE = "verify";
+        private const string ENCRYPT_VALUE = "encrypt";
+        private const string DECRYPT_VALUE = "decrypt";
+        private const string WRAP_KEY_VALUE = "wrapKey";
+        private const string UNWRAP_KEY_VALUE = "unwrapKey";
+        private const string DERIVE_KEY_VALUE = "deriveKey";
+        private const string DERIVE_BITS_VALUE = "deriveBits";
+
+        public static readonly KeyOperations ComputeDigitalSignature = new KeyOperations(SIGN_VALUE);
+        public static readonly KeyOperations VerifyDigitalSignature = new KeyOperations(VERIFY_VALUE);
+        public static readonly KeyOperations EncryptContent = new KeyOperations(ENCRYPT_VALUE);
+        public static readonly KeyOperations DecryptContentAndValidateDecryption = new KeyOperations(DECRYPT_VALUE);
+        public static readonly KeyOperations EncryptKey = new KeyOperations(WRAP_KEY_VALUE);
+        public static readonly KeyOperations DecryptKeyAndValidateDecryption = new KeyOperations(UNWRAP_KEY_VALUE);
+        public static readonly KeyOperations DeriveKey = new KeyOperations(DERIVE_KEY_VALUE);
+        public static readonly KeyOperations DeriveBits = new KeyOperations(DERIVE_BITS_VALUE);
 
         public IEnumerable<string> Operations;
 
@@ -46,7 +55,28 @@ namespace CreativeCode.JWK.KeyParts
 
         public object Deserialize(JToken jwkRepresentation)
         {
-            throw new NotImplementedException();
+            if (jwkRepresentation is null)
+                throw new NotSupportedException("Cannot deserialize null value");
+
+            var keyOperations = new List<KeyOperations>();
+            foreach(var operation in jwkRepresentation.Children())
+            {
+                var match = operation.ToString() switch
+                {
+                    SIGN_VALUE => ComputeDigitalSignature,
+                    VERIFY_VALUE => VerifyDigitalSignature,
+                    ENCRYPT_VALUE => EncryptContent,
+                    DECRYPT_VALUE => DecryptContentAndValidateDecryption,
+                    WRAP_KEY_VALUE => EncryptKey,
+                    UNWRAP_KEY_VALUE => DeriveKey,
+                    DERIVE_KEY_VALUE => DecryptKeyAndValidateDecryption,
+                    DERIVE_BITS_VALUE => DeriveBits,
+                    _  => null
+                };
+                keyOperations.Add(match);
+            }
+
+            return new KeyOperations(keyOperations);
         }
     }
 }
